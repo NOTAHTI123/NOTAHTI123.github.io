237db2dc82ddb7237be4326a9889f272f2aa3a7a
Notahti123 <ahtishamahmed1234@gmail.com>
2023-08-10 20:26:10 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("demo").innerHTML = "Hola JS";
+document.getElementById("demo").style.fontSize = "35px";
+// document.getElementById("demo").style.display = "none";
+
+function myfunction() {
+    document.getElementById("demo").innerHTML = "Para changed on click";
+    // document.write(5+6);
+    // window.alert("text updated");
+    console.log(10+1);
+}
